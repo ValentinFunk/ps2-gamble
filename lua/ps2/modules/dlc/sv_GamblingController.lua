9ec01af27753a0fc7aa5116ba4e8b5eaabce2ac8
Valentin Funk <funk.valentin@gmail.com>
2016-09-10 15:31:13 +0200

    Fix exploit

--- lua/ps2/modules/dlc/sv_GamblingController.lua
+++ lua/ps2/modules/dlc/sv_GamblingController.lua
@@ -2,28 +2,20 @@ Pointshop2.GamblingController = class( "Pointshop2.GamblingController" )
 Pointshop2.GamblingController:include( BaseController )
 
 function Pointshop2.GamblingController:canDoAction( ply, action )
-	return Promise.Resolve( )
+    if action == "startSpin" then
+	    return Promise.Resolve( )
+    else
+        return Promise.Reject( "Invalid action" )
+    end
 end
 
-function Pointshop2.GamblingController:startSpin( ply, bet )
-	if ply.PS2_Wallet.points < bet then
-		return Promise.Reject( "You cannot afford this!" )
-	end
-	
-	ply.lastSpin = ply.lastSpin or 0
-	if ply.lastSpin + 1.6 > CurTime() then
-		return Promise.Reject( "Hey, slow down a bit!" )
-	end
-	ply.lastSpin = CurTime( )
-	
-	ply:PS2_AddStandardPoints( -bet, "Gambling" )
-	
+function Pointshop2.GamblingController:spinLogic( bet )
 	local randomseeds = {
 		tostring( os.time( ) + math.random( os.time( ) ) ),
 		tostring( os.time( ) + math.random( os.time( ) ) ),
 		tostring( os.time( ) + math.random( os.time( ) ) )
 	}
-	
+
 	--Spin the wheel serverside
 	local endIcons = {}
 	for i = 1, 3 do
@@ -32,9 +24,9 @@ function Pointshop2.GamblingController:startSpin( ply, bet )
 			endIcons[i] = Pointshop2.Gambling:GetItemNumberForChance( math.random( 1, Pointshop2.Gambling.CHOICECOUNT ) )
 		end
 	end
-	
+
 	--Check for wins
-	local winNumber = endIcons[1] 
+	local winNumber = endIcons[1]
 	local won = true
 	for k, v in pairs( endIcons ) do
 		if v != winNumber then
@@ -42,7 +34,45 @@ function Pointshop2.GamblingController:startSpin( ply, bet )
 		end
 	end
 	local amountWon = bet * Pointshop2.Gambling.WinMultiplier[winNumber]
-	if won then
+
+	return won and amountWon
+end
+
+function Pointshop2.GamblingController:simulateSpinning( )
+	local balances = {}
+	for i = 1, 1000 do
+		local balance = 1000000
+		for i = 1, 10000 do
+			balance = balance - 100
+			balance = balance + ( Pointshop2.GamblingController:spinLogic( 100 ) or 0 )
+		end
+		print( "Performed 10000 spins, balance:", balance )
+		table.insert(balances, balance)
+	end
+	local average = LibK._.reduce(balances, 0, function(a, b) return a + b end) / #balances
+	print( "Average (100 * 10000 spins): ", average )
+end
+
+function Pointshop2.GamblingController:startSpin( ply, bet )
+	if ply.PS2_Wallet.points < bet then
+		return Promise.Reject( "You cannot afford this!" )
+	end
+
+    if not table.HasValue( Gambling.BetAmounts, bet ) then
+        return Promise.Reject( "Invalid Bet" )
+    end
+
+	ply.lastSpin = ply.lastSpin or 0
+	if ply.lastSpin + 1.6 > CurTime() then
+		return Promise.Reject( "Hey, slow down a bit!" )
+	end
+	ply.lastSpin = CurTime( )
+
+	ply:PS2_AddStandardPoints( -bet, "Gambling" )
+
+
+	local amountWon = self:spinLogic( bet )
+	if amountWon then
 		timer.Simple( 1.5, function( )
 			if not IsValid( ply ) then return end
 			if Pointshop2.GetSetting( "Pointshop 2 DLC", "GamblingSettings.GlobalSound" ) then
@@ -51,6 +81,6 @@ function Pointshop2.GamblingController:startSpin( ply, bet )
 			ply:PS2_AddStandardPoints( amountWon, "Gambling Earnings" )
 		end )
 	end
-	
-	return Promise.Resolve( randomseeds, won, amountWon )
+
+	return Promise.Resolve( randomseeds, amountWon and true, amountWon )
 end
